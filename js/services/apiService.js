8b01cfdb25a189ca5aa31507b5d5518929db3cd2
Deepa <deepasric28@gmail.com>
2016-02-04 13:12:23 +0800

    api update

--- js/services/apiService.js
+++ js/services/apiService.js
@@ -1,5 +1,8 @@
 angular.module('dashboard.services')
 .service('apiService',function($http){
+    
+//dashBoard details Get Api
+ 
     this.dashboardDetails = function() {
     return	$http.get("json/dashboard.json")
 		.success(function(response)
@@ -8,6 +11,9 @@ angular.module('dashboard.services')
    		 });
 
      }
+     
+ //dashBoard details update Api
+     
    this.dashboardUpdate = function(items) {
    	  console.log("items Updated :) ",  items);
     return	$http.post("json/dashboard.json")
@@ -18,8 +24,22 @@ angular.module('dashboard.services')
 
      }
 
+//dashBoard details Add Api
+
  	this.dashboardAdd = function() {
     return	$http.post("json/dashboard.json")
+		.success(function(response)
+   		 {
+ 			return response;
+   		 });
+
+     }
+     
+     
+//dashBoard details Detete Api
+     
+     this.dashboardDelete = function() {
+    return	$http.delete("json/dashboard.json")
 		.success(function(response)
    		 {
  			return response;
